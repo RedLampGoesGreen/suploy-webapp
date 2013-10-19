fdc6699cdadb2826b2e33e1280841f228e741f88
flower-pot <flowerpot@pondati.net>
2013-10-19 15:51:28 +0200

    Initial Commit
    -Added initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SuployWebapp::Application.config.secret_key_base = '5e49e466175121b0647fbf48b31d27a7a5c0f494cbfe8f1966d05327116da1b73eb9c3be2d4db1477e1a9704695b4f0f06d20fdef4fa0825561c003e67620402'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SuployWebapp::Application.config.session_store :cookie_store, key: '_suploy-webapp_session'
