e787a3230a69c1c5b418bd8da4950d1f477c3dfe
flower-pot <flowerpot@pondati.net>
2013-10-19 16:30:26 +0200

    Added omniauth with github

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,32 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable,
+    :confirmable, :omniauthable, :omniauth_providers => [:github]
+
+  validates :name,
+    :uniqueness => {
+    :case_sensitive => false
+  }
+
+  attr_accessor :confirmation_sent_at, :unconfirmed_email
+
+  def self.find_for_github_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    unless user
+      user = User.create(name: data["nickname"],
+                          email: data["email"],
+                          provider: access_token.provider,
+                          uid: access_token.uid,
+                          password: Devise.friendly_token[0,20],
+                        )
+    end
+    user
+  end
+
+  def skip_confirmation!
+    self.confirmed_at = Time.now
+  end
 end
