cc35814b3b73bc8c1d6716d3bcf490f2b58a5235
flower-pot <flowerpot@pondati.net>
2013-10-19 18:33:43 +0200

    Changed root route to the login page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ SuployWebapp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root :to => redirect("/users/sign_in")
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
